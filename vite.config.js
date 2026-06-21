d425294a24205b51032436fdff9368ea0f4d5b09
Sebastian <sebastian202502@gmail.com>
2026-06-20 23:27:18 -0500

    Arreglo GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/GANUS-FRONTEND/',
+  base: '/GANUS_FRONTENDO_V.2/',
   plugins: [react()],
 })
